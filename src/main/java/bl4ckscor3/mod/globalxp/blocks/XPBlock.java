5703b885efd7d20673b2d30f45c1aeaae7887439
Konlii <spbentley@gmail.com>
2018-01-20 04:24:58 -0600

    Merge pull request #7 from Konlii/master
    Adds TheOneProbe integration

--- src/main/java/bl4ckscor3/mod/globalxp/blocks/XPBlock.java
+++ src/main/java/bl4ckscor3/mod/globalxp/blocks/XPBlock.java
@@ -4,13 +4,19 @@ import java.util.Random;
 
 import bl4ckscor3.mod.globalxp.GlobalXP;
 import bl4ckscor3.mod.globalxp.network.packets.SPacketUpdateXPBlock;
+import bl4ckscor3.mod.globalxp.theoneprobe.TOPInfoProvider;
 import bl4ckscor3.mod.globalxp.tileentity.TileEntityXPBlock;
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.ProbeMode;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -20,7 +26,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
-public class XPBlock extends Block
+public class XPBlock extends Block implements TOPInfoProvider
 {
 	public XPBlock(Material materialIn)
 	{
@@ -122,4 +128,16 @@ public class XPBlock extends Block
 	{
 		return new TileEntityXPBlock();
 	}
+	
+    @Override
+    public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
+        TileEntity te = world.getTileEntity(data.getPos());
+        if(te instanceof TileEntityXPBlock)
+        {
+            TileEntityXPBlock tileentity = (TileEntityXPBlock) te;
+            probeInfo.horizontal().text(I18n.format("top.body", String.format("%.2f", tileentity.getStoredLevels())));
+            if(mode == ProbeMode.EXTENDED)
+                probeInfo.horizontal().text(I18n.format("top.extended", tileentity.getStoredXP()));
+        }
+    }
 }
