980a122f6902f6eb8c9c0c8403da38332a96d530
Geforce132 <Geforce132@gmail.com>
2019-01-04 14:53:19 -0600

    Added XP block recipe | added floating emerald into the middle of the XP block | updated version to v1.4 | fixed incorrect name for the XP block

--- src/main/java/bl4ckscor3/mod/globalxp/GlobalXP.java
+++ src/main/java/bl4ckscor3/mod/globalxp/GlobalXP.java
@@ -2,14 +2,14 @@ package bl4ckscor3.mod.globalxp;
 
 import bl4ckscor3.mod.globalxp.blockentities.XPBlockEntity;
 import bl4ckscor3.mod.globalxp.blocks.XPBlock;
+import bl4ckscor3.mod.globalxp.renderers.XPBlockRenderer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.client.render.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.networking.CustomPayloadPacketRegistry;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.block.BlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -20,21 +20,20 @@ public class GlobalXP implements ModInitializer {
 	
 	public static final String MODID = "globalxp";
 	public static final String NAME = "Global XP";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.4";
 	
 	public static final Identifier UPDATE_XP_BLOCK = new Identifier(MODID, "update_xp_block");
 	
 	public static final XPBlock XP_BLOCK = new XPBlock();
 
-	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MODID, "item_group"), () -> new ItemStack(XP_BLOCK));
 	public static final BlockEntityType<XPBlockEntity> XP_BLOCK_ENTITY = BlockEntityType.Builder.create(XPBlockEntity::new).method_11034(null);
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.BLOCK, XPBlock.ID, XP_BLOCK);
-		Registry.register(Registry.BLOCK_ENTITY, MODID + ":xp_block", XP_BLOCK_ENTITY);
-		Registry.register(Registry.ITEM, MODID + ":xp_block", new BlockItem(XP_BLOCK, new Item.Settings().itemGroup(ITEM_GROUP)));
-
+		Registry.register(Registry.BLOCK_ENTITY, XPBlock.ID, XP_BLOCK_ENTITY);
+		Registry.register(Registry.ITEM, XPBlock.ID, new BlockItem(XP_BLOCK, new Item.Settings().itemGroup(ItemGroup.MISC)));
+		BlockEntityRendererRegistry.INSTANCE.register(XPBlockEntity.class, new XPBlockRenderer());
 		
 		CustomPayloadPacketRegistry.CLIENT.register(UPDATE_XP_BLOCK, (packetContext, packetByteBuf) -> {
 			PlayerEntity player = packetContext.getPlayer();
---
+++ src/main/java/bl4ckscor3/mod/globalxp/renderers/XPBlockRenderer.java
@@ -0,0 +1,38 @@
+package bl4ckscor3.mod.globalxp.renderers;
+
+import org.lwjgl.opengl.GL11;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import bl4ckscor3.mod.globalxp.blockentities.XPBlockEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+@Environment(EnvType.CLIENT)
+public class XPBlockRenderer extends BlockEntityRenderer<XPBlockEntity> {
+
+	public void render(XPBlockEntity blockEntity, double x, double y, double z, float partialTicks, int int_1) {
+		double offset = Math.sin((blockEntity.getWorld().getTime() + partialTicks) * 1.0D / 8.0D) / 10.0D;
+		BakedModel model = MinecraftClient.getInstance().getItemRenderer().getModelMap().getModel(Items.EMERALD);
+
+		GlStateManager.enableRescaleNormal();
+		GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+		GlStateManager.enableBlend();
+		GlStateManager.disableLighting();
+		GlStateManager.blendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+		GlStateManager.pushMatrix();
+		GlStateManager.translated(x + 0.5D, y + 0.5D + offset, z + 0.5D);
+		GlStateManager.scalef(0.75F, 0.75F, 0.75F);
+		GlStateManager.rotatef((blockEntity.getWorld().getTime() + partialTicks) * 4.0F * (float)1.0F, 0.0F, 1.0F, 0.0F);
+		MinecraftClient.getInstance().getItemRenderer().renderItemAndGlow(new ItemStack(Items.EMERALD), model);
+		GlStateManager.popMatrix();
+		GlStateManager.disableRescaleNormal();
+		GlStateManager.disableBlend();
+	}
+
+}
