0208a750ba2828ae8f33043fc3dfcea101cf7c84
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-07-08 21:12:13 +0200

    use new features of forge 41.0.64
    also use the correct way to register block entity renderers

--- src/main/java/bl4ckscor3/mod/globalxp/ClientReg.java
+++ src/main/java/bl4ckscor3/mod/globalxp/ClientReg.java
@@ -1,22 +1,18 @@
 package bl4ckscor3.mod.globalxp;
 
 import bl4ckscor3.mod.globalxp.xpblock.client.XPBlockEntityRenderer;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @EventBusSubscriber(modid=GlobalXP.MOD_ID, value=Dist.CLIENT, bus=Bus.MOD)
 public class ClientReg
 {
 	@SubscribeEvent
-	public static void onFMLClientSetup(FMLClientSetupEvent event)
+	public static void onEntityRenderersRegisterRenderers(EntityRenderersEvent.RegisterRenderers event)
 	{
-		event.enqueueWork(() ->	BlockEntityRenderers.register(GlobalXP.XP_BLOCK_ENTITY_TYPE.get(), XPBlockEntityRenderer::new));
-		ItemBlockRenderTypes.setRenderLayer(GlobalXP.XP_BLOCK.get(), RenderType.cutout());
+		event.registerBlockEntityRenderer(GlobalXP.XP_BLOCK_ENTITY_TYPE.get(), XPBlockEntityRenderer::new);
 	}
 }
