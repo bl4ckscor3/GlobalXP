143ea31894d36f8daca69a38b087dd98b61bbe0a
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-08-19 13:28:35 +0200

    Add support for NeoForge's config screen

--- src/main/java/bl4ckscor3/mod/globalxp/ClientReg.java
+++ src/main/java/bl4ckscor3/mod/globalxp/ClientReg.java
@@ -3,13 +3,20 @@ package bl4ckscor3.mod.globalxp;
 import bl4ckscor3.mod.globalxp.xpblock.client.XPBlockEntityRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.EventBusSubscriber.Bus;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+@Mod(value = GlobalXP.MOD_ID, dist = Dist.CLIENT)
 @EventBusSubscriber(modid = GlobalXP.MOD_ID, value = Dist.CLIENT, bus = Bus.MOD)
 public class ClientReg {
-	private ClientReg() {}
+	public ClientReg(ModContainer modContainer) {
+		modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+	}
 
 	@SubscribeEvent
 	public static void onEntityRenderersRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
