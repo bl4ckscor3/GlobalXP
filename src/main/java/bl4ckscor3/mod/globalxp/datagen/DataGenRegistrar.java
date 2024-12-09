ba59687fc480d44ee6e8456312ca715935ce4a89
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-12-09 18:15:32 +0100

    Update to 1.21.4

--- src/main/java/bl4ckscor3/mod/globalxp/datagen/DataGenRegistrar.java
+++ src/main/java/bl4ckscor3/mod/globalxp/datagen/DataGenRegistrar.java
@@ -3,13 +3,9 @@ package bl4ckscor3.mod.globalxp.datagen;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 
 import bl4ckscor3.mod.globalxp.GlobalXP;
 import net.minecraft.DetectedVersion;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
 import net.minecraft.data.metadata.PackMetadataGenerator;
@@ -22,22 +18,19 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.EventBusSubscriber.Bus;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.data.event.GatherDataEvent.DataProviderFromOutputLookup;
 
 @EventBusSubscriber(modid = GlobalXP.MOD_ID, bus = Bus.MOD)
 public class DataGenRegistrar {
 	private DataGenRegistrar() {}
 
 	@SubscribeEvent
-	public static void onGatherData(GatherDataEvent event) {
-		DataGenerator generator = event.getGenerator();
-		PackOutput output = generator.getPackOutput();
-		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
-		generator.addProvider(event.includeServer(), new BlockTagGenerator(output, lookupProvider, event.getExistingFileHelper()));
-		generator.addProvider(event.includeServer(), new LootTableProvider(output, Set.of(), List.of(new SubProviderEntry(BlockLootTableGenerator::new, LootContextParamSets.BLOCK)), lookupProvider));
-		generator.addProvider(event.includeServer(), new RecipeGenerator.Runner(output, lookupProvider));
+	public static void onGatherData(GatherDataEvent.Client event) {
+		event.createProvider(BlockTagGenerator::new);
+		event.createProvider((DataProviderFromOutputLookup<LootTableProvider>) (output, lookupProvider) -> new LootTableProvider(output, Set.of(), List.of(new SubProviderEntry(BlockLootTableGenerator::new, LootContextParamSets.BLOCK)), lookupProvider));
+		event.createProvider(RecipeGenerator.Runner::new);
 		//@formatter:off
-		generator.addProvider(true, new PackMetadataGenerator(output)
+		event.createProvider(output -> new PackMetadataGenerator(output)
 				.add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("Global XP resources & data"),
 						DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
 						Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE)))));
