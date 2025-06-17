389b68b74f5831df60805b1da165817dc972ef23
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-06-17 22:57:18 +0200

    Update to 1.21.6

--- src/main/java/bl4ckscor3/mod/globalxp/GlobalXPClient.java
+++ src/main/java/bl4ckscor3/mod/globalxp/GlobalXPClient.java
@@ -2,14 +2,14 @@ package bl4ckscor3.mod.globalxp;
 
 import bl4ckscor3.mod.globalxp.xpblock.client.XPBlockEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.renderer.RenderType;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 
 public class GlobalXPClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		BlockRenderLayerMap.INSTANCE.putBlock(GlobalXP.XP_BLOCK, RenderType.cutout());
+		BlockRenderLayerMap.putBlock(GlobalXP.XP_BLOCK, ChunkSectionLayer.CUTOUT);
 		BlockEntityRenderers.register(GlobalXP.XP_BLOCK_ENTITY_TYPE, XPBlockEntityRenderer::new);
 	}
 }
